2835ce725dbe884f1f016e68a1972130dad2b175
TDavydov <timurl1kec0de@gmail.com>
2024-07-22 01:35:23 +0500

    Возвращаю 401 если юзер не авторизован

--- DevmateBackend/WebAPI/Program.cs
+++ DevmateBackend/WebAPI/Program.cs
@@ -3,7 +3,14 @@ using Microsoft.AspNetCore.Authentication.Cookies;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
-    .AddCookie();
+    .AddCookie(options =>
+    {
+        options.Events.OnRedirectToLogin = context =>
+        {
+            context.Response.StatusCode = 401;
+            return Task.CompletedTask;
+        };
+    });
 builder.Services.AddAuthorization();
 builder.Services.AddControllers();
 builder.Services.Configure<RouteOptions>(options => options.LowercaseUrls = true);
